3cbe37cc580b5b031e5081d8a9e7bee0ec978a28
Andrea Cappuccio <0xCAP@mail.com>
2020-07-08 15:54:02 +0200

    set up testing

---
+++ test/loot-table.test.ts
@@ -0,0 +1,24 @@
+import { LootTable } from "../src/loot-table";
+import Item from "../src/item";
+
+describe("LootTable", () => {
+  test("LootTable is defined", () => {
+    expect(LootTable).toBeDefined();
+  });
+
+  test("the loot table's total weight is calculated correctly", () => {
+    const lootTable: LootTable = new LootTable();
+
+    const items: Item[] = [
+      { id: "1", item: "Item one", dropRate: 10 },
+      { id: "2", item: "Item two", dropRate: 40 },
+    ];
+
+    lootTable.addItem(items[0]);
+    lootTable.addItem(items[1]);
+
+    expect(lootTable.totalWeight).toBe(
+      items.reduce((total, item) => (total += item.dropRate), 0)
+    );
+  });
+});
